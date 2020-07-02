6e4a4263a9a395639f89fde6ac188eb0d7fefed0
Eclismic <milcokats@gmail.com>
2020-07-02 13:29:59 +0200

    wordt genieten

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,40 @@
-import React, { Component } from 'react';
-import Form from './components/Form';
-import DisplayUsers from './components/DisplayUsers';
-import axios from 'axios';
+import React from 'react';
 import './App.css';
+import Header from './components/lay-out/Header/Header'
+import ContentBlockOne from './components/lay-out/ContentBlockOne/ContentBlockOne';
+import ContentBlockTwo from './components/lay-out/ContentBlockTwo/ContentBlockTwo';
+import Footer from './components/lay-out/Footer/Footer';
+import Locatie from './components/lay-out/Locatie/Locatie'
+import Boeking   from './components/lay-out/Boeking/Boeking'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+
 class App extends Component {
-  state = {
-    users: []
-  }
-
-  componentDidMount = () => {
-    this.fetchUsers();
-  };
-
-  fetchUsers = () => {
-    axios.get('/users')
-      .then((response) => {
-        const { users } = response.data;
-        this.setState({ users: [...this.state.users, ...users] })
-      })
-      .catch(() => alert('Error fetching new users'));
-  };
-
-
-  addUser = ({ name, position, company }) => {
-    this.setState({
-      users: [...this.state.users, { name, position, company }]
-    });
-  };
-
+ 
   render() {
     return (
-      <div className="App">
-        <Form addUser={this.addUser}/>
-        < DisplayUsers users={this.state.users} />
-
-      </div>
+      <Router>
+        <div className="App"> 
+          <div className="page-container">
+            <Header/>
+            <div className="content">
+                <Route exact path="/Tweepersoons" component={ContentBlockOne}/>
+                <Route exact path="/Vierpersoons" component={ContentBlockTwo}/>
+            </div>
+            <div className="map">
+                <Route exact path="/Locatie" component={Locatie}/>
+            </div>
+                <Route exact path="/Boeking" component={Boeking}/>
+            <div className="main-footer">
+                <Footer/>
+            </div>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
