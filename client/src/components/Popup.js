cb673363ba9891141c7920f68d60eaa8739d91ae
Milco <milcokats@gmail.com>
2020-08-06 21:12:38 +0200

    popup customizing

--- client/src/components/Popup.js
+++ client/src/components/Popup.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import checkmark from "../img/checkmark.svg"
 
 class Popup extends React.Component {
     render() {
       return (
         <div className='popup'>
           <div className='popup_inner'>
+          {(this.props.error === null) ? null : <img id="check-mark" src={checkmark}></img> }
             <h1>{this.props.text}</h1>
             <p>{this.props.error}</p>
             <div className="popup-button-wrapper">
